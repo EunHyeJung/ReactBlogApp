39900a36b873a6301f200660361f145ba8f84ed7
Eunhye Jung <ehye826@gmail.com>
2021-09-27 21:13:32 +0900

    add react state, component code

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,3 +1,4 @@
+/* eslint-disable */
 import logo from './logo.svg';
 import './App.css';
 import React, {useState} from 'react';
@@ -11,20 +12,43 @@ function App() {
   let style1 = {color : 'blue', fontSize : '30px;'}
 
   let [글제목, 글제목변경] = useState(['남자 코트 추천', '강남 우동 맛집']);  // state 데이터, state 데이터 변경함수가 a,b에 저장됨(ES6 destructing)
-  
+  let [좋아요, 좋아요변경] = useState(0);
+
+  function 제목변경() {
+    var newArray = [...글제목]; // 복사본(deep copy)
+    newArray[0] = '여자코드 추천';
+    글제목변경(newArray); // state를 아예 대체해주는 함수
+  }
   return (
     <div className="App">
       <div className="black-nav">
         <div style={ style1 }>개발 Blog</div>
       </div>
+      <button onClick={ 제목변경 }>버튼</button>
       <div className="list">
-        <h3> {글제목[0]}</h3>
+        <h3> {글제목[0]} <span onClick={() => { {좋아요변경(좋아요 + 1) } }}>👍</span>{좋아요}</h3>
         <p>2월 17일 발행</p>
+        <hr/>
       </div>        
       <div className="list">
         <h3> {글제목[1]}</h3>
         <p>2월 17일 발행</p>
-      </div>        
+        <hr/>
+      </div>
+      
+      <Modal></Modal>  
+              
+    </div>
+    
+  );
+}
+
+function Modal() {
+  return (
+    <div className="modal">
+        <h2>제목</h2>  
+        <p>날짜</p>
+        <p>상세내용</p>
     </div>
   );
 }

bb2cee6d290202623df06c16feefa350897b432f
Eunhye Jung <ehye826@gmail.com>
2021-09-30 22:59:30 +0900

    jsx에서 삼항연산자로 모달창 show on/off 해보기

--- blog/src/App.js
+++ blog/src/App.js
@@ -9,10 +9,11 @@ function App() {
   function func() {
       return 100;
   }
-  let style1 = {color : 'blue', fontSize : '30px;'}
+  let style1 = {color : 'blue', fontSize : '30px;'};
 
-  let [글제목, 글제목변경] = useState(['남자 코트 추천', '강남 우동 맛집']);  // state 데이터, state 데이터 변경함수가 a,b에 저장됨(ES6 destructing)
+  let [글제목, 글제목변경] = useState(['남자 코트 추천', '강남 우동 맛집', '파이썬독학']);  // state 데이터, state 데이터 변경함수가 a,b에 저장됨(ES6 destructing)
   let [좋아요, 좋아요변경] = useState(0);
+  let[modal, modal변경] = useState(false); // 모달창 열고닫는 스위치
 
   function 제목변경() {
     var newArray = [...글제목]; // 복사본(deep copy)
@@ -35,9 +36,19 @@ function App() {
         <p>9월 27일 발행</p>
         <hr/>
       </div>
+      <div className="list" > 
+        <h3 onClick={() => {modal변경(true) }}> {글제목[2]}</h3>
+        <p>9월 30일 발행</p>
+        <hr/>
+      </div>
       
-      <Modal></Modal>  
-              
+      {/* {
+        modal === true
+       ? <Modal/>
+       : null // 텅빈 HTML 이라는 뜻
+      } */}
+
+      <button>버튼</button>
     </div>
     
   );
